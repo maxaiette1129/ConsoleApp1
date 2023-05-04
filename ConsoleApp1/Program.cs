9c7e06f5108e56252cb6ffe8aaa6d582d353a401
maxaiette1129 <maxaiettec@gmail.com>
2023-05-04 19:40:42 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [][] jaggedArray = new int[50][];
+
+           Random rnd = new Random();
+
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                jaggedArray[i] = new int[rnd.Next(1,100)];
+                Console.WriteLine(jaggedArray[i].Length);
+
+                
+            }
+            
+     
+
+            
+
+
+
+        }
+    }
+}
